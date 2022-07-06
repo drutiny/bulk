de683eccdc1f7d6b7ced3a1e7b6972dc0c9c08a0
Josh Waihi <josh.waihi@acquia.com>
2022-07-07 10:54:56 +1200

    Fixed up WorkCommand to work within a Drutiny repo.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # bulk
+
+This extension adds bulk drutiny report running functions to Drutiny using
+a queue/worker architecture based on the AMPQ messaging protocol.
+
+## Spin up RabbitMQ with Docker.
+
+A quick way to get an AMPQ service is to spin up RabbitMQ in a docker container.
+This extensions ships a simple command that will run this for you:
+
+     drutiny bulk:run-queue-service
+
+This will run in the foreground and can be killed and exited using Ctrl-C. It is
+ephemeral so the queue will be lost if you exit the service.
+
+## Sending profile:run jobs to the queue.
+
+To send jobs to the queue, use the `bulk:queue` command. You can either send
+individual jobs to the queue one at a time or send a batch of jobs using a file.
+
+### Sending an individual jobs
+
+     drutiny bulk:queue my_custom_profile @sitealias.dev -f html -f csv
+
+The above command will send a job to the queue to `profile:run` the `my_custom_profile`
+profile against the `@sitealias.dev` target and render the results in html and csv formats.
+
+### Sending a batch of jobs
+
+    drutiny bulk:queue my_custom_profile --target-list=targets.txt -f html
+
+The above command will send a job to the queue for each line in `targets.txt`
+where each line is a target like `drush:@sitealias.dev`.
--- src/Commands/WorkCommand.php
+++ src/Commands/WorkCommand.php
@@ -6,6 +6,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
@@ -25,30 +27,36 @@ class WorkCommand extends Command
             // the command help shown when running the command with the "--help" option
             ->setName('bulk:work')
             ->setHelp('This command allows you to work targets to be run against a profile.')
-            ->addArgument('drutiny-dir', InputArgument::REQUIRED, 'The location where drutiny source is located.')
-            ->addArgument('reports-dir', InputArgument::REQUIRED, 'Where the reports should be written too.')
+            ->addOption('memory_limit', 'm', InputOption::VALUE_OPTIONAL, 'A php memory limit setting for the profile:run process.', '256M')
             ->addQueueOptions()
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $drutiny_bin = $GLOBALS['_composer_bin_dir'] . '/drutiny';
+
+        if (!file_exists($drutiny_bin)) {
+            $io->error("Cannot find a drutiny binary to run bulk commands.");
+            return Command::FAILURE;
+        }
+
         $connection = $this->getAMQPConnection($input);
         $channel = $connection->channel();
 
         $channel->queue_declare('profile_run_error', false, false, false, false);
 
-        $working_directory = getcwd();
 
-        $callback = function ($msg) use ($working_directory, $channel, $input, $output) {
-            chdir($input->getArgument('drutiny-dir'));
+        $callback = function ($msg) use ($drutiny_bin, $channel, $input, $output) {
             $payload = json_decode($msg->body);
 
-            $command = 'php -d memory_limit=256M ./vendor/bin/drutiny profile:run %s %s -o %s --exit-on-severity=16';
+            $command = 'php -d memory_limit=%s %s profile:run %s %s --exit-on-severity=16';
             $args = [
+              escapeshellarg($input->getOption('memory_limit')),
+              escapeshellarg($drutiny_bin),
               escapeshellarg($payload->profile),
               escapeshellarg($payload->target),
-              escapeshellarg($input->getArgument('reports-dir')),
             ];
             foreach ($payload->format as $format) {
                 $command .= ' -f %s';
@@ -60,13 +68,12 @@ class WorkCommand extends Command
             $payload->worker_time = time();
             passthru($command, $return_var);
 
-            chdir($working_directory);
             $msg->ack();
 
             if ($return_var !== 0) {
                 $payload->exit_code = $return_var;
                 $output->writeln("[ERROR] {$payload->target} on {$payload->profile} exited with error code ($return_var).");
-                $channel->basic_publish(new AMQPMessage(json_encode($payload)), '', 'profile_run_error');
+                // $channel->basic_publish(new AMQPMessage(json_encode($payload)), '', 'profile_run_error');
             }
         };
 
