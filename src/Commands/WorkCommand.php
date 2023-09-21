fd451b78c803b6fa9b2f4b41a8983436da211975
Josh Waihi <josh.waihi@acquia.com>
2023-09-21 12:10:29 +1200

    Update work command,

--- src/Commands/WorkCommand.php
+++ src/Commands/WorkCommand.php
@@ -3,6 +3,7 @@
 namespace Drutiny\Bulk\Commands;
 
 use DateTime;
+use Drutiny\Bulk\Message\MessageStatus;
 use Drutiny\Bulk\Message\ProfileRun;
 use Drutiny\Bulk\QueueService\QueueServiceFactory;
 use Drutiny\Console\Command\DrutinyBaseCommand;
@@ -43,9 +44,8 @@ class WorkCommand extends DrutinyBaseCommand
             ->setHelp('This command allows you to work targets to be run against a profile.')
             ->addArgument(
                 'queue_name',
-                InputArgument::OPTIONAL,
+                InputArgument::REQUIRED | InputArgument::IS_ARRAY,
                 'The queue name to work. Defaults to profile:run.',
-                'profile:run'
             )
             ->addOption(
                 'memory_limit', 
@@ -75,18 +75,65 @@ class WorkCommand extends DrutinyBaseCommand
             $io->error("Cannot find a drutiny binary to run bulk commands.");
             return Command::FAILURE;
         }
-        $date = new \DateTime();
-        $output->writeln($date->format('H:i:s') . ' Listening to ' . $input->getArgument('queue_name') . ' for new messages...');
-        $output->writeln(['']);
 
-        $this->queueServiceFactory
-            ->load($input->getOption('queue-service'))
-            ->consume($input->getArgument('queue_name'), fn(ProfileRun $message) => $message->execute(
-                input: $input,
-                output: $output,
-                bin: $drutiny_bin,
-                logger: $this->logger,
-            ));
+        $queueService = $this->queueServiceFactory->load($input->getOption('queue-service'));
+
+        // Track sequential retries.
+        $sequential_retries = 0;
+        $queues = $input->getArgument('queue_name');
+        $priority = 0;
+        
+        do {
+            $date = new \DateTime();
+            $output->writeln(strtr($date->format('H:i:s') . " Consuming messages from queue", [
+                'queue' => $queues[$priority]
+            ]));
+
+            $last_message_status = $queueService->consume(
+                queue_name: $queues[$priority], 
+                callback: fn(ProfileRun $message) => $message->execute(
+                    input: $input,
+                    output: $output,
+                    bin: $drutiny_bin,
+                    logger: $this->logger,
+                ),
+                // Only loop if queue priority is 0 and the queue delivered a message.
+                //continue: fn(MessageStatus $status) => $priority == 0 && $status != MessageStatus::NONE
+                continue: function (MessageStatus $status) use ($priority, $sequential_retries) {
+                    if ($status == MessageStatus::RETRY) {
+                        $sequential_retries++;
+                    }
+                    else {
+                        $sequential_retries = 0;
+                    }
+                    if ($sequential_retries >= 10) {
+                        return false;
+                    }
+                    return $priority == 0 && $status != MessageStatus::NONE;
+                }
+            );
+
+            // If the queue checked was not top priority go back and check the top queue again.
+            if ($priority > 0) {
+                $priority = 0;
+            }
+            // Exit because queue is empty. Check next priority.
+            elseif ($last_message_status == MessageStatus::NONE) {
+                $this->logger->notice("No messages in {queue}. Checking queue in next priority.", ['queue' => $queues[$priority]]);
+                $priority++;
+            }
+
+            // Reset priority when no queue for a given priority.
+            if (!isset($queues[$priority])) {
+                $priority = 0;
+            }
+
+            if ($sequential_retries >= 10) {
+                $this->logger->warning("Processed 10 messages in a row that all returned a RETRY status. Perhaps there is something wrong with this worker. Exiting.");
+                return Command::FAILURE;
+            }
+        }
+        while (true);
 
         return Command::SUCCESS;
     }
